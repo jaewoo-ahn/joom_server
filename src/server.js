74a66d01dee7ca9f9d6696e171df276d4a320b1e
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-01-25 16:49:19 +0900

    feat : front setup

--- src/server.js
+++ src/server.js
@@ -6,5 +6,6 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => res.render("home"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.listen(3000);
